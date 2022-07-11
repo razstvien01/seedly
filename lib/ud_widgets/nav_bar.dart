555063d012cf7cf9a065d4d1ea093e5130e307de
razstvien01 <nicolenevanz.aricayos@gmail.com>
2022-07-11 10:26:39 +0800

    navigation bar done

--- lib/ud_widgets/nav_bar.dart
+++ lib/ud_widgets/nav_bar.dart
@@ -36,25 +36,31 @@ class _NavBarState extends State<NavBar> {
           selectedIndex: index,
           height: 60,
           backgroundColor: Colors.grey[100],
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          animationDuration: Duration(seconds: 2),
           onDestinationSelected: (index) => setState(() {
             this.index = index;
           }),
           destinations: [
             NavigationDestination(
-              icon: Icon(Icons.home), label: 'Home',
+              icon: Icon(Icons.home_outlined), label: 'Home',
+              selectedIcon: Icon(Icons.home),
             ),
             
             NavigationDestination(
-              icon: Icon(Icons.shopping_cart), label: 'Cart',
+              icon: Icon(Icons.shopping_cart_outlined), label: 'Cart',
+              selectedIcon: Icon(Icons.shopping_cart),
               
             ),
             
             NavigationDestination(
-              icon: Icon(Icons.messenger_rounded), label: 'Chat',
+              icon: Icon(Icons.messenger_outline), label: 'Chat',
+              selectedIcon: Icon(Icons.messenger),
             ),
             
             NavigationDestination(
               icon: Icon(Icons.account_circle_outlined), label: 'Profile',
+              selectedIcon: Icon(Icons.account_circle),
             ),
           ],
         ),
